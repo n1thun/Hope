12f3bc24d925f43009ef3fb07de1fce993cdca03
nithunx <nithun@gmail.com>
2016-10-22 11:16:05 -0500

    test run successfull

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from . import views
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('dashboard.urls'))
+    url(r'^datetest/', views.current_datetime, name="datetest")
+
 ]
