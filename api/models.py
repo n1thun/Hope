815f08033654ec736506263383aa4ad51a8c1fe1
David <dgore7@gmail.com>
2016-10-22 14:02:06 -0500

    added a whole bunch of models

--- api/models.py
+++ api/models.py
@@ -2,12 +2,30 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Foreclosure(models.Model):
-    defendant = models.CharField(max_length=128)
-    address = models.CharField(max_length=128)
-    saleDate = models.DateField()
-    zip = models.CharField(max_length=6)
-    contact = models.CharField(max_length=128)
+class Person(models.Model):
+    name = models.CharField(max_length=512)
+    address = models.CharField(max_length=512)
+    zip = models.CharField(max_length=11)
     city = models.CharField(max_length=128)
-    deedOfTrustAmount = models.DecimalField()
-    createDate = models.DateField()
+    age = models.IntegerField()
+    dob = models.DateField()
+    coc = models.BooleanField()
+
+class Foreclosure(models.Model):
+    defendant = models.ForeignKey('Person', on_delete=models.CASCADE)
+    address = models.CharField(max_length=512)
+    sale_date = models.DateField()
+    zip = models.CharField(max_length=11)
+    city = models.CharField(max_length=128)
+    deed_of_trust_amount = models.DecimalField()
+    create_date = models.DateField()
+
+    def __str__(self):
+        return "{} forclosed on {}".format(self.defendent, self.create_date)
+
+class ForclosureStatusHistory(models.Model):
+    foreclosure = models.ForeignKey('Foreclosure', on_delete=models.CASCADE)
+    status = models.CharField(max_length=512)
+
+    def __str__(self):
+        return "{}".format(self.status)
