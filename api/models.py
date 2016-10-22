e3f9bc7213005fa02c7b3e507e7ff5a93a484620
David <dgore7@gmail.com>
2016-10-22 11:49:03 -0500

    started api

--- api/models.py
+++ api/models.py
@@ -2,4 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Foreclosure(models.Model):
+    defendant = models.CharField(max_length=128)
+    address = models.CharField(max_length=128)
+    saleDate = models.DateField()
