df17361735ec7bc4bbd27a4de91d8c1383a0c551
David <dgore7@gmail.com>
2016-10-22 12:08:02 -0500

    created forclosure model

--- api/models.py
+++ api/models.py
@@ -6,3 +6,8 @@ class Foreclosure(models.Model):
     defendant = models.CharField(max_length=128)
     address = models.CharField(max_length=128)
     saleDate = models.DateField()
+    zip = models.CharField(max_length=6)
+    contact = models.CharField(max_length=128)
+    city = models.CharField(max_length=128)
+    deedOfTrustAmount = models.DecimalField()
+    createDate = models.DateField()
